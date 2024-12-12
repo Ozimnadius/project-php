23ef1bd790713d683b084e113d7434f38717579b
Mikle Seregin <mikhail-seregin@yandex.ru>
2024-12-12 12:01:30 +0300

    update

--- src/components/btn/index.php
+++ src/components/btn/index.php
@@ -1,12 +1,4 @@
-<?/* $default = array(
-    "href" => "#",
-    "class" => "btn-default",
-    "text" => "Кнопка"
-);
 
-$result = array_merge($default, $params);
-*/?>
-
-<a href="<?= $arResult["href"]; ?>" class="btn <?= $arResult["class"]; ?>">
+<a href="<?=$arResult["href"]; ?>" class="btn <?= $arResult["class"]; ?>">
     <span><?= $arResult["text"]; ?></span>
 </a>
