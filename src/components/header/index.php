c580b66840c4cdd1cf4e978d0f8271273a96f44c
Mikle Seregin <mikhail-seregin@yandex.ru>
2025-04-17 13:57:38 +0300

    Update

--- src/components/header/index.php
+++ src/components/header/index.php
@@ -1,3 +1,23 @@
 <header class="header">
-    <? component('menu', [],'src/data/menu.json'); ?>
+    <div class="header__logo">
+        <a href="/">
+            <img src="/upload/images/logo.webp"
+                 width="118"
+                 height="61"
+                 alt=""
+                 loading="lazy"
+            >
+        </a>
+    </div>
+    <div class="header__menu">
+        <? component('menu', [],'src/data/menu.json'); ?>
+    </div>
+    <div class="header__socials">
+        <? component('socials', [],'src/data/socials.json'); ?>
+    </div>
+    <div class="header__phone">
+        <a href="tel:+7(495)123-45-67">
+            + 7 (495) 123-45-67
+        </a>
+    </div>
 </header>
